210db0f73078d69b1d41f8978c7c643abbca7b01
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2026-05-10 02:53:27 +0300

    feat: implementing the interceptors logic and put the token in any api request call

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 import { routes } from './app.routes';
 
@@ -9,7 +10,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideAnimations(),
   ],
 };
